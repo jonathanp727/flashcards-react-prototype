f5c207e75f5ac60a24641c6af98694ce300e0289
Jonathan Phipps <jonathanmp25@gmail.com>
2019-07-02 13:22:00 -0500

    Add redux

---
+++ src/Services/store.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+const rootReducer = combineReducers({
+
+});
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    thunk,
+    logger,
+  ),
+);
+
+
+export default store;
